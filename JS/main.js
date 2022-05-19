56c5e21313b0f9f11c8c901f513d38667543b991
just-hope-netizen <hopepeter007@gmail.com>
2022-05-19 15:52:03 +0100

    added swiper-slide-text class to a element

---
+++ JS/main.js
@@ -0,0 +1,34 @@
+const hamburger = document.querySelector(".hamburger")
+const navMenu = document.querySelector(".nav-menu")
+
+
+ hamburger.addEventListener('click',() =>{
+     hamburger.classList.toggle("active")
+     navMenu.classList.toggle("active")
+ })
+
+ document.querySelectorAll(".nav-link").forEach(n => n.
+    addEventListener("click" ,()=>{
+    hamburger.classList.remove("active")
+    navMenu.classList.remove("active")
+}) )
+
+
+//swiper js section
+
+var swiper = new Swiper(".mySwiper", {
+    effect: "coverflow",
+    grabCursor: true,
+    centeredSlides: true,
+    slidesPerView: "auto",
+    coverflowEffect: {
+      rotate: 50,
+      stretch: 0,
+      depth: 100,
+      modifier: 1,
+      slideShadows: true,
+    },
+    pagination: {
+      el: ".swiper-pagination",
+    },
+  });
---
+++ README.md
@@ -0,0 +1,8 @@
+# Mental-Check
+This is a mental help app that enables user log in and authentication, journal ,meditate and know learn about mental health and how to see
+help.
+
+###Live site
+--link
+
+## Technologies
